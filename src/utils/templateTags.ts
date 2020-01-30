aa8e7dd6814f34bb792b1835c9aec4edb7e02325
Christopher Satchell <satchell028@gmail.com>
2020-01-30 15:02:49 +0100

    Fix template tag capitalization

--- src/utils/templateTags.ts
+++ src/utils/templateTags.ts
@@ -5,9 +5,17 @@ const capitalize = new TemplateTag({
   onSubstitution(str: string) {
     return str.charAt(0).toUpperCase() + str.slice(1);
   },
+  onEndResult(str: string) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  },
 });
 
-const insertEmoji = new TemplateTag(replaceResultTransformer(/:[A-Z_]*:/g, (match) => EMOJIS[match.slice(1, match.length - 1)] || 'Unknown emoji'));
+const insertEmoji = new TemplateTag(
+  replaceResultTransformer(
+    /:[A-Z_]*:/g,
+    (match) => EMOJIS[match.slice(1, match.length - 1)] || 'Unknown emoji',
+  ),
+);
 
 export {
   capitalize,
