f1d353f528afbe126a4689e9fbb15d124bff5c72
Christopher Satchell <satchell028@gmail.com>
2020-02-12 15:20:40 +0100

    Log stacktraces
    No clue why I expected them to be added automatically.

--- src/logger.ts
+++ src/logger.ts
@@ -14,26 +14,31 @@ const {
 const logFormat = combine(
   timestamp(),
   errors({ stack: true }),
-  printf((info) => stripIndent`
+  printf(({
+    timestamp: ts,
+    level,
+    group,
+    message,
+    stack,
+  }) => stripIndent`
     ${oneLine`
-      ${info.timestamp}
-      [${info.level.toUpperCase()}]:
-      ${(info.group ? `[${info.group}] ` : '')}
-    `} ${info.message}
+      ${ts}
+      [${level.toUpperCase()}]:
+      ${(group ? `[${group}] ` : '')}
+    `} ${message} ${stack ? `\n${stack}` : ''}
   `),
 );
 
 const logger = createLogger({
   level: env.logging.level,
-  format: logFormat,
   transports: [
     new DailyRotateFile({
       datePattern: 'YYYY-MM-DD',
       dirname: 'logs',
       filename: '%DATE%.log',
+      format: logFormat,
       handleExceptions: true,
       level: 'info',
-      format: logFormat,
       maxFiles: '30d',
       utc: true,
     }),
