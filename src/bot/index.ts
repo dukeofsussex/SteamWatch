f6ecaf2a105c8714cba17bca64b2d19ea2d1c8f8
Christopher Satchell <satchell028@gmail.com>
2020-02-16 12:48:56 +0100

    Add bot sharding

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,114 +1,13 @@
-import { oneLine } from 'common-tags';
-import { RateLimitInfo } from 'discord.js';
-import { readdirSync } from 'fs';
-import { join } from 'path';
-import db from '../db';
-import env from '../env';
-import logger from '../logger';
-import SteamWatchClient from './structures/SteamWatchClient';
-import MariaDBProvider from './commando/providers/MariaDB';
-import MentionListType from './commando/types/MentionList';
-import Steam from '../steam/Steam';
+import Bot from './Bot';
 
-export default class Bot {
-  private client: SteamWatchClient;
+const bot = new Bot();
+bot.startAsync();
 
-  constructor() {
-    this.client = new SteamWatchClient({
-      commandPrefix: env.bot.prefix,
-      invite: env.bot.invite,
-      messageCacheMaxSize: 1,
-      owner: env.bot.owners,
-      steam: new Steam(),
-      unknownCommandResponse: false,
-    });
-  }
+const SIGNALS: NodeJS.Signals[] = ['SIGHUP', 'SIGINT', 'SIGTERM', 'SIGUSR2'];
 
-  async startAsync() {
-    if (!env.debug) {
-      await db.migrate.latest();
-      await db.seed.run();
-      logger.info('Database migrated and seeded');
-    }
-
-    logger.info('Database ready');
-
-    this.client.setProvider(new MariaDBProvider());
-
-    this.client.registry
-      .registerDefaultTypes()
-      .registerType(MentionListType)
-      .registerDefaultGroups()
-      .registerGroups([['apps', 'Apps']])
-      .registerCommandsIn({
-        filter: /^([^.].*)\.(js|ts)$/,
-        dirname: join(__dirname, 'commands'),
-      });
-
-    this.client.once('ready', async () => {
-      logger.info({
-        group: 'Discord',
-        message: `Logged in as '${this.client.user.tag}'`,
-      });
-
-      this.setActivity();
-    });
-
-    this.client.setInterval(() => this.setActivity(), 300000);
-
-    if (env.debug) {
-      this.client.on('debug', (message) => logger.debug({ group: 'Discord', message }));
-    }
-
-    this.client.on('commandError', (_, err, message) => logger.error({
-      group: 'Commando',
-      message: `${message.content} : ${err}`,
-    }));
-    this.client.on('disconnect', () => logger.info({ group: 'Discord', message: 'Disconnected' }));
-    this.client.on('error', (err) => logger.error({ group: 'Discord', message: err }));
-    this.client.on('warn', (message) => logger.warn({ group: 'Discord', message }));
-    this.client.on('rateLimit', (info: RateLimitInfo) => logger.warn({
-      group: 'Discord',
-      message: `Limit of ${info.limit} for ${info.method} ${info.path}`,
-    }));
-    this.client.on('reconnecting', () => logger.info({
-      group: 'Discord',
-      message: 'Reconnecting',
-    }));
-    this.client.on('resume', (replayed: number) => logger.info({
-      group: 'Discord',
-      message: `Resuming, replayed ${replayed} events`,
-    }));
-
-    const eventFiles = readdirSync(join(__dirname, 'events'))
-      .filter((file) => !file.endsWith('.map'));
-    const eventHandlers = await Promise
-      .all(eventFiles.map((file) => import(join(__dirname, 'events', file))));
-    eventFiles.forEach((file, i) => {
-      this.client.on(file.split('.')[0], eventHandlers[i].default);
-    });
-
-    this.client.login(env.bot.token);
-  }
-
-  async stopAsync() {
-    await this.client.destroy();
-    this.client.removeAllListeners();
-  }
-
-  private async setActivity() {
-    const count = await db.count('* AS count')
-      .from('app')
-      .innerJoin('app_watcher', 'app_watcher.app_id', 'app.id')
-      .first()
-      .then((res: any) => res.count);
-
-    this.client.user.setActivity(
-      oneLine`
-        ${count} apps for ${this.client.guilds.size} guilds
-        | ${this.client.commandPrefix}${this.client.commandPrefix.length > 1 ? ' ' : ''}help
-      `,
-      { type: 'WATCHING' },
-    );
-  }
+for (let i = 0; i < SIGNALS.length; i += 1) {
+  const event = SIGNALS[i];
+  process.on(event, async () => {
+    await bot.stopAsync();
+  });
 }
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,48 @@
-import Bot from './bot';
+import { Shard, ShardingManager } from 'discord.js';
+import { existsSync } from 'fs';
+import { join } from 'path';
+import env from './env';
 import Manager from './watcher';
+import logger from './logger';
 
-const SIGNALS: NodeJS.Signals[] = ['SIGHUP', 'SIGINT', 'SIGTERM', 'SIGUSR1', 'SIGUSR2'];
-let bot: Bot;
+const SIGNALS: NodeJS.Signals[] = ['SIGHUP', 'SIGINT', 'SIGTERM', 'SIGUSR2'];
+let shardingManager: ShardingManager;
 let manager: Manager;
 
 if (!process.argv.includes('--no-bot')) {
-  bot = new Bot();
-  bot.startAsync();
+  let botFilePath = join(__dirname, 'bot', 'index.js');
+  if (!existsSync(botFilePath)) {
+    botFilePath = join(__dirname, 'bot', 'index.ts');
+  }
+
+  shardingManager = new ShardingManager(botFilePath, {
+    token: env.bot.token,
+  });
+
+  shardingManager.on('launch', (shard: Shard) => {
+    logger.info({
+      group: 'Shard',
+      message: `#${shard.id} launched`,
+    });
+    shard.on('death', () => logger.warn({
+      group: 'Shard',
+      message: `#${shard.id} died`,
+    }))
+      .on('ready', () => () => logger.info({
+        group: 'Shard',
+        message: `#${shard.id} ready`,
+      }))
+      .on('disconnect', () => logger.info({
+        group: 'Shard',
+        message: `#${shard.id} disconnected`,
+      }))
+      .on('reconnecting', () => logger.info({
+        group: 'Shard',
+        message: `#${shard.id} reconnecting`,
+      }));
+  });
+
+  shardingManager.spawn();
 }
 
 if (!process.argv.includes('--no-watcher')) {
@@ -23,8 +58,14 @@ process.on('unhandledRejection', (err) => {
 for (let i = 0; i < SIGNALS.length; i += 1) {
   const event = SIGNALS[i];
   process.on(event, async () => {
-    await bot?.stopAsync();
+    logger.info({
+      group: 'Shard',
+      message: 'All shutting down',
+    });
     manager?.stop();
-    process.exit(0);
+    setTimeout(() => {
+      logger.destroy();
+      process.exit(0);
+    }, 5000);
   });
 }
