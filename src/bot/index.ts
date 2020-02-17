27a3bffe1f1c1dc6b3a4107f454b7984556be3a1
Christopher Satchell <satchell028@gmail.com>
2020-02-17 14:10:57 +0100

    Let the bot handle unhandled rejections

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -5,6 +5,11 @@ bot.startAsync();
 
 const SIGNALS: NodeJS.Signals[] = ['SIGHUP', 'SIGINT', 'SIGTERM', 'SIGUSR2'];
 
+process.on('unhandledRejection', (err) => {
+  throw err;
+});
+
+
 for (let i = 0; i < SIGNALS.length; i += 1) {
   const event = SIGNALS[i];
   process.on(event, async () => {
