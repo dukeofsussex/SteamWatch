b5abc05b80c2157864049ce15bb51e4c44ca9859
Christopher Satchell <satchell028@gmail.com>
2020-03-31 19:28:25 +0200

    Only fetch channel webhooks if necessary

--- src/bot/events/webhookUpdate.ts
+++ src/bot/events/webhookUpdate.ts
@@ -1,30 +1,33 @@
 import { TextChannel, Webhook } from 'discord.js';
 import db from '../../db';
 
-// Used to prevent webhooks from being duplicated
-const webhookBeingSet: {
+// Prevent duplicate webhooks
+const webhookQueue: {
   [key: string]: boolean;
 } = {};
 
 export default async function webhookUpdate(channel: TextChannel) {
-  if (webhookBeingSet[channel.id]) {
+  if (webhookQueue[channel.id]) {
     return;
   }
 
-  const results = await Promise.all([
-    channel.fetchWebhooks(),
-    db.select({ id: 'webhook_id' })
-      .from('channel_webhook')
-      .innerJoin('app_watcher', 'app_watcher.channel_id', 'webhook.id')
-      .where('id', channel.id)
-      .first(),
-  ]);
+  const dbWebhook = await db.select({ id: 'webhook_id' })
+    .from('channel_webhook')
+    .innerJoin('app_watcher', 'app_watcher.channel_id', 'channel_webhook.id')
+    .where('channel_webhook.id', channel.id)
+    .first();
 
-  if (!results[1] || results[0].map((hook: Webhook) => hook.id).includes(results[1].id)) {
+  if (!dbWebhook) {
     return;
   }
 
-  webhookBeingSet[channel.id] = true;
+  const webhooks = await channel.fetchWebhooks();
+
+  if (webhooks.map((hook: Webhook) => hook.id).includes(dbWebhook.id)) {
+    return;
+  }
+
+  webhookQueue[channel.id] = true;
 
   const webhook = await channel.createWebhook(
     channel.client.user.username,
@@ -35,5 +38,5 @@ export default async function webhookUpdate(channel: TextChannel) {
   await db('channel_webhook').update({ webhookId: webhook.id, webhookToken: webhook.token })
     .where('id', channel.id);
 
-  delete webhookBeingSet[channel.id];
+  delete webhookQueue[channel.id];
 }
